556fb4590b7bdd295f6bdbbd506c405a8a30783d
gabriel.stanica <gabriel.stanica@ISW-W19>
2018-02-14 10:17:20 +0200

    Add project files.

---
+++ RenameFiles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace RenameFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\gastanica\Desktop\OCR_Chris";
+            var namePattern = "TestImage";
+            int count = 0;
+
+            var filesInDir = Directory.GetFiles(path);
+            foreach(var file in filesInDir)
+            {
+                FileInfo files = new FileInfo(file);
+                files.Rename(String.Format("{0}{1}.png",namePattern,count));
+                count++;
+            }
+        }
+
+    }
+    public static class Extension
+    {
+        public static void Rename(this FileInfo fileInfo, string newName)
+        {
+            fileInfo.MoveTo(fileInfo.Directory.FullName + "\\" + newName);
+        }
+    }
+}
